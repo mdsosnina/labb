a72d12bb47da43a38c3d831bf2dcfb8ece0a3431
mdsosnina <130696477+mdsosnina@users.noreply.github.com>
2023-06-11 00:42:56 +0300

    Add files via upload

---
+++ lab3_a.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+using namespace std;
+class Detail
+{
+protected:
+    Detail() = default;
+    virtual ~Detail() = default;
+};
+class Assembly : public Detail
+{
+protected:
+    friend void FillVector(vector<Assembly>* vec); 
+    explicit Assembly(const string& detail_info) : detail(detail_info) {
+        cout << "Assembly constructor is called." << endl;
+    }
+    const string& getDetail() const { 
+        return detail;
+    }
+private:
+    string detail;
+};
+void FillVector(vector<Assembly>* vec) { 
+    for (int i = 0; i < 5; ++i)
+    {
+        auto a = unique_ptr<Assembly>(new Assembly("Detail #" + to_string(i)));
+        vec->push_back(move(*a));
+    }
+}
+vector<Assembly> vec;
+int main() {
+    FillVector(&vec);
+    for (auto i = vec.begin(); i != vec.end(); i++)
+    {
+        cout << i->getDetail() << endl; 
+    }
+    return 0;
+}
